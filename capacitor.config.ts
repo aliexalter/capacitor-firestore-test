6167613131924efeba27410b74d5208ebfa688bf
aliexalter <ali@softwarestudio.co.uk>
2024-02-14 16:42:33 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Capacitor Firestore Test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
